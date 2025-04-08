3c8e59efd4cfa31e601b774eeddc6847d423fc2a
quiroz-d01 <quiroz.d01@mymail.sxu.edu>
2025-04-08 00:35:28 -0500

    added achievement page. still need to implement firebase functions

---
+++ frontend/app/src/main/java/com/example/homepage/ui/RemoveBlacklist.kt
@@ -0,0 +1,18 @@
+package com.yourpackage.name
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.homepage.R
+
+class RemoveBlacklist : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_remove_blacklist)
+
+
+
+    }
+}
