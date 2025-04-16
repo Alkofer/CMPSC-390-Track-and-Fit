6f8d6d3ca28a54482650683b804bf7c14559fdce
FinninGH <131936047+FinninGH@users.noreply.github.com>
2025-04-15 19:45:18 -0500

    Connected all the pages we have in use, generating a new workout when requested

--- frontend/app/src/main/java/com/example/homepage/ExercisePage.kt
+++ frontend/app/src/main/java/com/example/homepage/ExercisePage.kt
@@ -3,10 +3,14 @@ package com.example.homepage
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.homepage.R.id.Sets_Text
 import com.example.homepage.R.id.mainButton
+import com.example.homepage.model.Workout
 import com.example.homepage.ui.MainActivity
+import org.json.JSONObject
 
 class ExercisePage : AppCompatActivity() {
     private lateinit var workoutType: String
@@ -16,18 +20,26 @@ class ExercisePage : AppCompatActivity() {
 
     private lateinit var workoutDesc: String    //To change the description of the exercise
     private lateinit var workoutName: String    //To change the displayed workout name
-    private var setsDone = 0                    //To keep track of sets completed
-    private lateinit var setsText: String       //To use when displaying sets completed
+    var setsDone = 0                    //To keep track of sets completed
+    var setsText: String = "Sets done: "       //To use when displaying sets completed
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_exercise_page)
 
-        val button = findViewById<Button>(R.id.mainButton);
+        val button = findViewById<Button>(R.id.mainButton); //button to take the user back to the main page
         button.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        val Sets_Text = findViewById<TextView>(R.id.Sets_Text)
+
+        val buttonPlus = findViewById<Button>(R.id.SetPlus_Button); //button to increase the amount of sets done for tracking purposes
+        button.setOnClickListener {
+            val setsDone = setsDone + 1
+            Sets_Text.setText(setsText + setsDone)
         }
 
         fun fetchWorkouts(workoutType: String) { //removes blacklisted workouts
